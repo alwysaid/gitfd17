2320bcdd709a8ddc2a65e16c2eb37e598bfc5923
alwysaid <164992429+alwysaid@users.noreply.github.com>
2024-05-17 21:47:33 +0700

    catatan fd17

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * DESIGN PATTERN
+ * 
+ * Desain pattern itu kaya membatik
+ * 
+ * Design pattern membuat lebih terstruktur dan mudah dipahami
+ * 
+ * Design Pattern : 
+ * 
+ * Creational, Structural, Behavioral
+ * 
+ * Khusus JS Lebih Spesifik : Creational, Struktural, Behavioral, Conccurency, Architectural Design Pattern
+ * 
+ * Paling sering dipake : Architectural Design Pattern
+ * Turunan 
+ * Model View Controller (MVC) dan Client Server Pattern
+ * 
+ * Analogi : 
+ * Bikin rumah => sebelum bikin tentukan arsitekturnya
+ * Begitupun Website
+ * 
+ * ada 2 bentuk software : 
+ * 
+ * Monolithic = client server disatuin, one code based 
+ * Biasnya untuk projek skala kecil ex : personal page, landing page
+ * 
+ * + = mudah dikembangkan, mudah deploy, waktu runtime sesuai, ga repot, hemat, waktu efisien
+ * - = basis kode susah dipahami, semakin besar web semakin lambat, semakin besar apps, semakin berat servernya. ga efisien untuk di scale up
+ * 
+ * 
+ * 
+ * Microservice
+ * 
+ * punya banyak, dipisah pisahin foldernya,
+ * digunakan untuk team yg banyak dan aplikasi besar
+ * bukan untuk menggantikan monolith seutuhnya
+ * 
+ * + = untuk apps kompleks, server banyak, tiap microservice lebih kecil sehingga mudah dipahami
+ * - = pembuatan awal rumit, kerumitan deploy
+ * 
+ * 
+ * 
+ * 
+ */
